1c7795ad13cc3bd334f9517380e5349a7b33bc58
Dani Abe <dnlaber@gmail.com>
2023-11-07 01:38:35 +0000

    aggregated by orders_id and date

--- models/intermediate/int_sales_margin.sql
+++ models/intermediate/int_sales_margin.sql
@@ -6,7 +6,7 @@ SELECT
     quantity, 
     CAST(purchase_price AS FLOAT64) AS purchase_price, 
     ROUND(s.quantity*CAST(p.purchase_price AS FLOAT64),2) AS purchase_cost,
-    s.revenue - ROUND(s.quantity*CAST(p.purchase_price AS FLOAT64),2) AS margin
+    ROUND(s.revenue -s.quantity*CAST(p.purchase_price AS FLOAT64),2) AS margin
 FROM {{ref("stg_raw__sales")}} s
 LEFT JOIN {{ref("stg_raw__product")}} p 
 		USING (products_id)
